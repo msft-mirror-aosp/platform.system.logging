5eae18dc033804219caba52774e575f60a44303e
Elliott Hughes <enh@google.com>
2025-03-24 07:20:00 -0700

    <android/log.h>: a little more documentation clarity.
    Re-order the functions so related pairs are adjacent.
    
    Also be explicit about functions that log to the main buffer,
    and add the missing "see also"s for the more general functions.
    
    Change-Id: I838a3eeea959a9292a97fdd4b160566d5ed843a5

--- liblog/include/android/log.h
+++ liblog/include/android/log.h
@@ -94,57 +94,6 @@ typedef enum android_LogPriority {
 } android_LogPriority;
 
 /**
- * Writes the constant string `text` to the log,
- * with priority `prio` (one of the `android_LogPriority` values) and tag `tag`.
- *
- * @return 1 if the message was written to the log, or -EPERM if it was not; see
- * __android_log_is_loggable().
- */
-int __android_log_write(int prio, const char* tag, const char* text);
-
-/**
- * Writes a formatted string to the log,
- * with priority `prio` (one of the `android_LogPriority` values) and tag `tag`.
- *
- * The details of formatting are the same as for
- * [printf(3)](http://man7.org/linux/man-pages/man3/printf.3.html).
- *
- * @return 1 if the message was written to the log, or -EPERM if it was not; see
- * __android_log_is_loggable().
- */
-int __android_log_print(int prio, const char* tag, const char* fmt, ...)
-    __attribute__((__format__(printf, 3, 4)));
-
-/**
- * Equivalent to __android_log_print(), but taking a `va_list`.
- * (If __android_log_print() is like printf(), this is like vprintf().)
- *
- * @return 1 if the message was written to the log, or -EPERM if it was not; see
- * __android_log_is_loggable().
- */
-int __android_log_vprint(int prio, const char* tag, const char* fmt, va_list ap)
-    __attribute__((__format__(printf, 3, 0)));
-
-/**
- * Writes an assertion failure to the log (as `ANDROID_LOG_FATAL`) and to
- * stderr, before calling
- * [abort(3)](http://man7.org/linux/man-pages/man3/abort.3.html).
- *
- * If `fmt` is non-null, `cond` is unused. If `fmt` is null, the string
- * `Assertion failed: %s` is used with `cond` as the string argument.
- * If both `fmt` and `cond` are null, a default string is provided.
- *
- * Most callers should use
- * [assert(3)](http://man7.org/linux/man-pages/man3/assert.3.html) from
- * `&lt;assert.h&gt;` instead, or the `__assert` and `__assert2` functions
- * provided by bionic if more control is needed. They support automatically
- * including the source filename and line number more conveniently than this
- * function.
- */
-void __android_log_assert(const char* cond, const char* tag, const char* fmt, ...)
-    __attribute__((__noreturn__)) __attribute__((__format__(printf, 3, 4)));
-
-/**
  * Identifies a specific log buffer for __android_log_buf_write()
  * and __android_log_buf_print().
  */
@@ -185,6 +134,17 @@ static inline bool __android_log_id_is_valid(log_id_t log_id) {
 }
 
 /**
+ * Writes the constant string `text` to the main log buffer,
+ * with priority `prio` (one of the `android_LogPriority` values) and tag `tag`.
+ *
+ * See __android_log_buf_write() to write to a different log buffer.
+ *
+ * @return 1 if the message was written to the log, or -EPERM if it was not; see
+ * __android_log_is_loggable().
+ */
+int __android_log_write(int prio, const char* tag, const char* text);
+
+/**
  * Writes the string `text` to the log buffer `log_id` (one of the `log_id_t` values),
  * with priority `prio` (one of the `android_LogPriority` values) and tag `tag`.
  *
@@ -195,6 +155,21 @@ static inline bool __android_log_id_is_valid(log_id_t log_id) {
  * __android_log_is_loggable().
  */
 int __android_log_buf_write(int log_id, int prio, const char* tag, const char* text);
+
+/**
+ * Writes a formatted string to the main log buffer,
+ * with priority `prio` (one of the `android_LogPriority` values) and tag `tag`.
+ *
+ * The details of formatting are the same as for
+ * [printf(3)](http://man7.org/linux/man-pages/man3/printf.3.html).
+ *
+ * See __android_log_buf_print() to write to a different log buffer.
+ *
+ * @return 1 if the message was written to the log, or -EPERM if it was not; see
+ * __android_log_is_loggable().
+ */
+int __android_log_print(int prio, const char* tag, const char* fmt, ...)
+    __attribute__((__format__(printf, 3, 4)));
 
 /**
  * Writes a formatted string to the log buffer `log_id` (one of the `log_id_t` values),
@@ -211,6 +186,35 @@ int __android_log_buf_write(int log_id, int prio, const char* tag, const char* t
  */
 int __android_log_buf_print(int log_id, int prio, const char* tag, const char* fmt, ...)
     __attribute__((__format__(printf, 4, 5)));
+
+/**
+ * Equivalent to __android_log_print(), but taking a `va_list`.
+ * (If __android_log_print() is like printf(), this is like vprintf().)
+ *
+ * @return 1 if the message was written to the log, or -EPERM if it was not; see
+ * __android_log_is_loggable().
+ */
+int __android_log_vprint(int prio, const char* tag, const char* fmt, va_list ap)
+    __attribute__((__format__(printf, 3, 0)));
+
+/**
+ * Writes an assertion failure to the main log buffer
+ * with priority `ANDROID_LOG_FATAL` -- and also to stderr -- before calling
+ * [abort(3)](http://man7.org/linux/man-pages/man3/abort.3.html).
+ *
+ * If `fmt` is non-null, `cond` is unused. If `fmt` is null, the string
+ * `Assertion failed: %s` is used with `cond` as the string argument.
+ * If both `fmt` and `cond` are null, a default string is provided.
+ *
+ * Most callers should use
+ * [assert(3)](http://man7.org/linux/man-pages/man3/assert.3.html) from
+ * `&lt;assert.h&gt;` instead, or the `__assert` and `__assert2` functions
+ * provided by bionic if more control is needed. They support automatically
+ * including the source filename and line number more conveniently than this
+ * function.
+ */
+void __android_log_assert(const char* cond, const char* tag, const char* fmt, ...)
+    __attribute__((__noreturn__)) __attribute__((__format__(printf, 3, 4)));
 
 /**
  * Logger data struct used for writing log messages to liblog via __android_log_write_logger_data()
@@ -243,6 +247,7 @@ struct __android_log_message {
  * Prototype for the 'logger' function that is called for every log message.
  */
 typedef void (*__android_logger_function)(const struct __android_log_message* log_message);
+
 /**
  * Prototype for the 'abort' function that is called when liblog will abort due to
  * __android_log_assert() failures.
